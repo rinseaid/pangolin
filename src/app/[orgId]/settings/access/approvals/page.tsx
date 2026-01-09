9fb3a687a3ff2d279ad602b1da9343200aec538b
Fred KISSIE <fredkiss3@gmail.com>
2026-01-09 02:20:08 +0100

    🚧 working on the approval feed

--- src/app/[orgId]/settings/access/approvals/page.tsx
+++ src/app/[orgId]/settings/access/approvals/page.tsx
@@ -1,5 +1,49 @@
-export interface ApprovalFeedPageProps {}
+import SettingsSectionTitle from "@app/components/SettingsSectionTitle";
+import { internal } from "@app/lib/api";
+import { authCookieHeader } from "@app/lib/api/cookies";
+import { getCachedOrg } from "@app/lib/api/getCachedOrg";
+import OrgProvider from "@app/providers/OrgProvider";
+import type { ListApprovalsResponse } from "@server/private/routers/approvals";
+import type { GetOrgResponse } from "@server/routers/org";
+import type { AxiosResponse } from "axios";
+import { getTranslations } from "next-intl/server";
 
-export default function ApprovalFeedPage(props: ApprovalFeedPageProps) {
-    return <></>;
+export interface ApprovalFeedPageProps {
+    params: Promise<{ orgId: string }>;
+}
+
+export default async function ApprovalFeedPage(props: ApprovalFeedPageProps) {
+    const params = await props.params;
+
+    let approvals: ListApprovalsResponse["approvals"] = [];
+    const res = await internal
+        .get<
+            AxiosResponse<ListApprovalsResponse>
+        >(`/org/${params.orgId}/approvals`, await authCookieHeader())
+        .catch((e) => {});
+
+    if (res && res.status === 200) {
+        approvals = res.data.data.approvals;
+    }
+
+    let org: GetOrgResponse | null = null;
+    const orgRes = await getCachedOrg(params.orgId);
+
+    if (orgRes && orgRes.status === 200) {
+        org = orgRes.data.data;
+    }
+
+    const t = await getTranslations();
+
+    return (
+        <>
+            <SettingsSectionTitle
+                title={t("accessApprovalsManage")}
+                description={t("accessApprovalsDescription")}
+            />
+            <OrgProvider org={org}>
+                <h1>Orgs</h1>
+            </OrgProvider>
+        </>
+    );
 }
